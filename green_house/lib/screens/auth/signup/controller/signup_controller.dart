29b0c0a0ebe95f6a816e211babed5d1293139723
Alex101499 <alherreras10@gmail.com>
2022-08-31 22:34:54 -0500

    Ambiente de desarrollo

--- green_house/lib/screens/auth/signup/controller/signup_controller.dart
+++ green_house/lib/screens/auth/signup/controller/signup_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -11,33 +12,37 @@ class SignupController extends GetxController {
 
   RxBool isPassObscure = true.obs;
   RxBool isConfirmPassObscure = true.obs;
-  var isLoading=false.obs;
+  var isLoading = false.obs;
+  var email = '';
+  var pass = '';
 
-  Future<dynamic>signUp() async {
+  Future<dynamic> signUp() async {
+    CollectionReference users = FirebaseFirestore.instance.collection('user');
     try {
       isLoading(true);
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: emailController.text.trim(),
-          password: passController.text.trim(),
-      ).then((value) async {
-    successSnackBar('Account created successfully');
-    emailController.clear();
-    passController.clear();
-    confirmPassController.clear();
-    Get.off(LoginScreen());
-    isLoading(false);
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passController.text.trim(),
+      )
+          .then((value) async {
+        successSnackBar('Account created successfully');
+        users.add({'email': emailController.text, 'pass': passController.text});
+        emailController.clear();
+        passController.clear();
+        confirmPassController.clear();
+        Get.off(LoginScreen());
+        isLoading(false);
       });
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-    errorSnackBar('The password provided is too weak.');
+        errorSnackBar('The password provided is too weak.');
       } else if (e.code == 'email-already-in-use') {
-    errorSnackBar('The account already exists for that email.');
-      }else{
+        errorSnackBar('The account already exists for that email.');
+      } else {
         errorSnackBar('Something went Wrong, Please try again.');
       }
       isLoading(false);
     }
-
   }
-
 }
