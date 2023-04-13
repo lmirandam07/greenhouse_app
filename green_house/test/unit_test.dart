f1f065486a1de06a9a58d77570b5dbf2dac59048
Alexander Herrera <alherreras10@gmail.com>
2023-04-13 11:41:29 -0500

    Configurando las pruebas unitarias

--- green_house/test/unit_test.dart
+++ green_house/test/unit_test.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:green_house/models/home_model.dart';
 import 'package:green_house/services/firestore_services/firestore_services.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-
+import 'package:green_house/models/home_members.dart';
 import 'unit_test.mocks.dart';
 
 class FirebaseTest extends Mock implements FirestoreService {}
@@ -12,6 +13,21 @@ Future<void> main() async {
   late MockFirebaseTest firabaseRepo;
   setUpAll(() {
     firabaseRepo = MockFirebaseTest();
+  });
+
+  test('create home', () async {
+    final home = HomeModel(
+        home_name: 'Casa Prueba',
+        owner_id: '1',
+        activated: true,
+        ubication: {'lat': 0.0, 'lon': 0.0});
+    final members = HomeMembersModel(
+        member_id: '1', member_role: 'owner', member_status: 'accepted');
+    final List<HomeMembersModel>? listMembers = [];
+    when(firabaseRepo.createHome(home, listMembers))
+        .thenAnswer((realInvocation) async => 'Success');
+
+    expect(await firabaseRepo.createHome(home, listMembers), 'Success');
   });
 
   test('validate User Exist', () async {
