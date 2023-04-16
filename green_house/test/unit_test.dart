d7ca921ede3bd66621a74b09e340f8b65d8bb6c1
Alex101499 <alherreras10@gmail.com>
2023-04-16 18:55:00 -0500

    Creando las pruebas unitarias de obtencion de informacion

--- green_house/test/unit_test.dart
+++ green_house/test/unit_test.dart
@@ -59,8 +59,10 @@ Future<void> main() async {
         member_id: '1', member_role: 'owner', member_status: 'accepted');
     when(firebaseRepo.createHomeMembers('1', members))
         .thenThrow(Exception('No se pudo crear los miembros del hogar'));
-    expect(()=>firebaseRepo.createHomeMembers('1', members), throwsException);
-    expect(()=>firebaseRepo.createHomeMembers('1', members), throwsA(predicate((dynamic e) =>
+    expect(() => firebaseRepo.createHomeMembers('1', members), throwsException);
+    expect(
+        () => firebaseRepo.createHomeMembers('1', members),
+        throwsA(predicate((dynamic e) =>
             e is Exception &&
             e.toString() ==
                 'Exception: No se pudo crear los miembros del hogar')));
@@ -113,4 +115,68 @@ Future<void> main() async {
         .thenAnswer((realInvocation) async => false);
     expect(await firebaseRepo.validateUserExist('lmiranda'), isFalse);
   });
+
+  test('Usuario existe en el hogar', () async {
+    when(firebaseRepo.validateUserHomeExist('1', 'aherrera'))
+        .thenAnswer((realInvocation) async => true);
+    expect(await firebaseRepo.validateUserHomeExist('1', 'aherrera'), true);
+  });
+  test('Usuario no existe en el hogar', () async {
+    when(firebaseRepo.validateUserHomeExist('1', 'lmiranda'))
+        .thenAnswer((realInvocation) async => false);
+    expect(await firebaseRepo.validateUserHomeExist('1', 'lmiranda'), false);
+  });
+
+  test('Exeption en Usuario existe en el hogar', () async {
+    when(firebaseRepo.validateUserHomeExist('', 'aherrera'))
+        .thenThrow(Exception('El parametro homeId no puede ser Null'));
+    expect(() => firebaseRepo.validateUserHomeExist('', 'aherrera'),
+        throwsException);
+  });
+  test('Obtener informacion del usuario', () async {
+    when(firebaseRepo.getUserData('ahrrera'))
+        .thenAnswer((realInvocation) => 'Exito');
+    expect(firebaseRepo.getUserData('ahrrera'), 'Exito');
+  });
+
+  test('Exception Obtener informacion del usuario', () async {
+    when(firebaseRepo.getUserData('lmiranda'))
+        .thenThrow(Exception('No existe ese usuario'));
+    expect(() => firebaseRepo.getUserData('lmiranda'), throwsException);
+  });
+
+  test('Obtener casas del usuario', () async {
+    when(firebaseRepo.getUserHomeList()).thenAnswer((realInvocation) async => [
+          {
+            'home_id': '1',
+            'home_name': 'Casa Prueba',
+            'owner_id': '1',
+            'ubicacion': {'lat': 0.0, 'long': 0.0}
+          }
+        ]);
+    expect(await firebaseRepo.getUserHomeList(), isList);
+    expect(await firebaseRepo.getUserHomeList(), isNotEmpty);
+  });
+
+  test('Lista de casas del usuario vacias', () async {
+    when(firebaseRepo.getUserHomeList())
+        .thenAnswer((realInvocation) async => []);
+    expect(await firebaseRepo.getUserHomeList(), isList);
+    expect(await firebaseRepo.getUserHomeList(), isEmpty);
+  });
+
+  test('Exception obtener casas de usuarios', () async {
+    when(firebaseRepo.getUserHomeList()).thenThrow(
+        Exception('No se pudo realizar la operacion. Vuelve a intentarlo'));
+    expect(() => firebaseRepo.getUserHomeList(), throwsException);
+  });
+
+  test('Obtener cantidad de usuarios por casa', () async {
+    when(firebaseRepo.getUserHomeCount('x7vbc')).thenAnswer((realInvocation) async=> {'count':2,'status':'accepted'});
+    expect(await firebaseRepo.getUserHomeCount('x7vbc'), isMap);
+    expect(await firebaseRepo.getUserHomeCount('x7vbc'), isNotEmpty);
+
+  });
+
+
 }
